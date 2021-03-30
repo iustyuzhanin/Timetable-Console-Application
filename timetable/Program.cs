72342b161798f875223cb726499659e41e0ed41d
ustyuzhanininal <inalustuzanin@gmail.com>
2021-03-30 15:15:00 +0500

    Добавил фу-ию для автоматического расположения по центру текста. Добавил комменты. Работает инструмент - Изменить. Удалить и Добавить в процессе.

--- timetable/Program.cs
+++ timetable/Program.cs
@@ -17,7 +17,7 @@ namespace timetable
                 "ИНФОРМЕР"
             };
 
-        // Информация для директора
+        // Информация из модуля Директор
         static string[] directorInformation =
             {
                 "Учителя",
@@ -27,14 +27,45 @@ namespace timetable
                 "Время звонков"
             };
 
+        // Индекс информаци из модуля Директор
+        static int directorInformationIndex = 0;
+
+        // Информация по каждому подразделу директора
+        static string[] informationEdit;
+
+        // Имя файла, который нужно открыть для модуля Директор
+        static string informationSelect = "";
+
+        // интрументы модуля Диреткор
+        static string[] tools =
+            {
+                "ДОБАВИТЬ",
+                "ИЗМЕНИТЬ",
+                "УДАЛИТЬ"
+            };
+
+
+        /// <summary>
+        /// Для определения центра по ширине консоли для заданной строки
+        /// </summary>
+        /// <param name="str">Строка</param>
+        /// <returns></returns>
+        static int PositionLeft(string str)
+        {
+            int left = Console.WindowWidth / 2 - str.Length / 2;
+            return left;
+        }
+
         /// <summary>
         /// Заголовок - РАСПИСАНИЕ УРОКОВ
         /// </summary>
         static void TitleTimetable()
         {
-            Console.SetCursorPosition(25, 5);
+            string str = "РАСПИСАНИЕ УРОКОВ";
+            int left = PositionLeft(str);
+            Console.SetCursorPosition(left, 5);
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("РАСПИСАНИЕ УРОКОВ");
+            Console.WriteLine(str);
         }
 
         /// <summary>
@@ -88,14 +119,34 @@ namespace timetable
                     case ConsoleKey.Enter:
                         isWorking = false;
 
+                        // выбираем в какой раздел заходить, зависит от текущего меню
+                        // глвное меню
                         if (menu == modules)
                         {
+                            // авторизация
                             Authorization(active);
+                        }
+                        //// меню в модуле Директор
+                        //else if (menu == directorInformation)
+                        //{
+                        //    // список информации в модуле Директор
+                        //    ModuleDirectorInformation(active);
+                        //}
+                        //меню по каждому подразделу директора
+                        else if (menu == informationEdit)
+                        {
+                            // страница изменения объектов
+                            ModuleDirectorInformationEdit(active);
                         }
                         else if (menu == directorInformation)
                         {
-                            ModuleDirectorInformation(active);
+                            ModuleDirectorTools(active);
                         }
+                        else if (menu == tools)
+                        {
+                            ModuleDirectorToolsInformation(active);
+                        }
+
 
                         break;
                     case ConsoleKey.UpArrow:
@@ -123,22 +174,24 @@ namespace timetable
         static void Authorization(int module)
         {
             Console.Clear();
-
-            int xPos = 26;
-
             TitleTimetable();
 
-            Console.SetCursorPosition(27, 13);
-            Console.WriteLine("АВТОРИЗАЦИЯ");
+            string str = "АВТОРИЗАЦИЯ";
+            int left = PositionLeft(str);
+            Console.SetCursorPosition(left, 13);
+            Console.WriteLine(str);
 
-            Console.SetCursorPosition(xPos, 16);
-            Console.WriteLine("Введите логин:");
-            Console.SetCursorPosition(xPos, 17);
+            str = "Введите логин:";
+            left = PositionLeft(str);
+            Console.SetCursorPosition(left, 16);
+            Console.WriteLine(str);
+
+            Console.SetCursorPosition(left, 17);
             string login = Console.ReadLine();
 
-            Console.SetCursorPosition(xPos, 19);
+            Console.SetCursorPosition(left, 19);
             Console.WriteLine("Введите пароль:");
-            Console.SetCursorPosition(xPos, 20);
+            Console.SetCursorPosition(left, 20);
 
             ConsoleKeyInfo info;
             var password = "";
@@ -183,54 +236,116 @@ namespace timetable
             string loginFile = File.ReadLines("users.txt").Skip(lineNumber).Take(1).First();
             string passwordFile = File.ReadLines("users.txt").Skip(++lineNumber).Take(1).First();
 
-            Console.SetCursorPosition(23, 22);
+            //Console.SetCursorPosition(23, 22);
             if (login==loginFile && password==passwordFile)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Вход выполнен успешно.");
+                str = "Вход выполнен успешно";
+                left = PositionLeft(str);
+                Console.SetCursorPosition(left, 22);
+                Console.WriteLine(str);
                 Console.ReadKey(true);
                 ModuleDirector();
             }
             else
             {
-                Console.SetCursorPosition(20, 22);
+                str = "Неправильный логин или пароль";
+                left = PositionLeft(str);
+                Console.SetCursorPosition(left, 22);
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.BackgroundColor = ConsoleColor.Black;
-                Console.WriteLine("Неправильный логин или пароль.");
-                Console.SetCursorPosition(25, 23);
-                Console.WriteLine("Введите еще раз.");
+                Console.WriteLine(str);
+
+                str = "Введите еще раз";
+                left = PositionLeft(str);
+                Console.SetCursorPosition(left, 23);
+                Console.WriteLine(str);
                 Console.BackgroundColor = ConsoleColor.DarkCyan;
                 Console.ReadKey(true);
                 Authorization(module);
             }
 
             //Console.WriteLine(loginFile);
-            //Console.WriteLine(passwordFile);
-           
-            //Console.Clear();
-            //Select(modules, 28, 15);
+            //Console.WriteLine(passwordFile);         
         }
 
+        /// <summary>
+        /// Меню модуля Директор
+        /// </summary>
         static void ModuleDirector()
         {
             Console.Clear();
             TitleTimetable();
 
-            Console.SetCursorPosition(20, 13);
-            Console.WriteLine("ДОБАВИТЬ/ИЗМЕНИТЬ ИНФОРМАЦИЮ:");
+            string str = "ИНФОРМАЦИЯ";
+            int left = PositionLeft(str);
+            Console.SetCursorPosition(left, 13);
+            Console.WriteLine(str);
 
-            Select(directorInformation, 28, 16);
+            str = directorInformation[1];
+            left = PositionLeft(str);
+
+            Select(directorInformation, left, 16);
         }
 
-        static void ModuleDirectorInformation(int information)
+        /// <summary>
+        /// Меню инструментов
+        /// </summary>
+        /// <param name="tool">Индекс инструмента</param>
+        static void ModuleDirectorTools(int tool)
+        {
+            Console.Clear();
+            TitleTimetable();
+            directorInformationIndex = tool;
+
+            string str = $"{directorInformation[tool].ToUpper()}";
+            int left = PositionLeft(str);
+            Console.SetCursorPosition(left, 13);
+            Console.WriteLine(str);
+
+            str = tools[0];
+            left = PositionLeft(str);
+            Select(tools, left, 16);
+        }
+
+        static void ModuleDirectorToolsInformation(int tool)
         {
             Console.Clear();
             TitleTimetable();
 
-            Console.SetCursorPosition(27, 13);
-            Console.WriteLine($"{directorInformation[information].ToUpper()}:");
+            string str = $"{tools[tool].ToUpper()}";
+            int left = PositionLeft(str);
+            Console.SetCursorPosition(left, 13);
+            Console.WriteLine(str);
 
-            string informationSelect = "";
+            switch (tool)
+            {
+                case 0:
+                    ModuleDirectorInformationAdd(directorInformationIndex);
+                    break;
+                case 1:
+                    ModuleDirectorInformation(directorInformationIndex);
+                    break;
+                case 2:
+
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Список объектов модуля Директор
+        /// </summary>
+        /// <param name="information">Индекс выбранной информации из модуля Директор</param>
+        static void ModuleDirectorInformation(int information)
+        {
+            //Console.Clear();
+            TitleTimetable();
+
+            //string str = $"{tools[information].ToUpper()}";
+            //int left = PositionLeft(str);
+            //Console.SetCursorPosition(left, 13);
+            //Console.WriteLine(str);
+
             switch (information)
             {
                 case 0:
@@ -250,14 +365,16 @@ namespace timetable
                     break;
             }
 
-            int linesCount = System.IO.File.ReadAllLines($@"director/{informationSelect}.txt").Length;
+            // кол-во строк в файле
+            int linesCount = File.ReadAllLines($@"director/{informationSelect}.txt").Length;
 
+            // читаем из файла
             FileStream informationStream = new FileStream($@"director/{informationSelect}.txt", FileMode.Open);
             StreamReader informationReader = new StreamReader(informationStream, Encoding.Default);
 
             int countIndex = 0;
 
-            string[] informationEdit = new string[linesCount];
+            informationEdit = new string[linesCount];
 
             while (!informationReader.EndOfStream)
             {
@@ -268,8 +385,79 @@ namespace timetable
             informationReader.Close();
             informationStream.Close();
 
+            string str = informationEdit[0];
+            int left = PositionLeft(str);
+            Select(informationEdit, left, 16);
+
+        }
+
+        /// <summary>
+        /// Страница изменения объектов
+        /// </summary>
+        /// <param name="informationEditCurrent">Индекс объекта, который надо изменить</param>
+        static void ModuleDirectorInformationEdit(int informationEditCurrent)
+        {
+            Console.Clear();
+            TitleTimetable();
+
+            string str = $"ИЗМЕНИТЬ: {informationEdit[informationEditCurrent]}";
+            int left = PositionLeft(str);
+            Console.SetCursorPosition(left, 15);
+            Console.WriteLine(str);
+
+            str = $"НА: ";
+            Console.SetCursorPosition(left, 17);
+            Console.Write(str);
+            string informationEditNew = Console.ReadLine();
+
+            foreach (var information in informationEdit)
+            {
+                if (information == informationEdit[informationEditCurrent])
+                {
+                    informationEdit[informationEditCurrent] = informationEditNew;
+                }
+            }
+
+            // записываем в файл измененные данные
+            FileStream informationStream = new FileStream($@"director/{informationSelect}.txt", FileMode.Open);
+            StreamWriter informationWriter = new StreamWriter(informationStream, Encoding.Default);
+
+            foreach (var information in informationEdit)
+            {
+                informationWriter.WriteLine($"{information}");
+            }
+
+            informationWriter.Close();
+            informationStream.Close();
+
+            Console.Clear();
             Select(informationEdit, 23, 16);
 
+        }
+
+        /// <summary>
+        /// Страница добавления объектов
+        /// </summary>
+        /// <param name="informationAddCurrent">Индекс объекта, который надо добавить</param>
+        static void ModuleDirectorInformationAdd(int informationAddCurrent)
+        {
+            Console.Clear();
+            TitleTimetable();
+
+            string str = $"ДОБАВИТЬ: ";
+            int left = PositionLeft(str);
+            Console.SetCursorPosition(left, 15);
+            Console.Write(str);
+            string informationAdd = Console.ReadLine();
+
+            // записываем в файл новые данные
+            //FileStream informationStream = new FileStream($@"director/{informationSelect}.txt", FileMode.Open);
+            //StreamWriter informationWriter = new StreamWriter(informationStream, Encoding.Default);
+
+            //informationWriter.WriteLine(informationAdd);
+
+            //informationWriter.Close();
+            //informationStream.Close();
         }
 
         static void Main(string[] args)
@@ -278,7 +466,9 @@ namespace timetable
             Console.BackgroundColor = ConsoleColor.DarkCyan;
             Console.Clear();
 
-            Select(modules, 29, 15);
+            string str = modules[0];
+            int left = PositionLeft(str);
+            Select(modules, left, 15);
 
             //Console.WriteLine($"ВЫ {modules[index]}");
             Console.ReadKey(true);
